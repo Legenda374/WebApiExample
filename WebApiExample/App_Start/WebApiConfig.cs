289105b043d331502ecb7012e2569f138d68098f
Артур Оксенюк <okssartur@gmail.com>
2019-07-18 20:54:47 +0300

    Add controllers

--- WebApiExample/App_Start/WebApiConfig.cs
+++ WebApiExample/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace WebApiExample
@@ -14,9 +15,12 @@ namespace WebApiExample
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
